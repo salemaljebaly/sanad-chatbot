2e4d58c731c1fa961b95b482417b256295c1eee0
Abdullah Habberrih <abdul2013ly@gmail.com>
2025-03-22 15:26:13 +0200

    feat: add a controller to test gemini instead of whatsapp

--- src/openai/openai.module.ts
+++ src/openai/openai.module.ts
@@ -3,9 +3,11 @@ import { OpenaiService } from './openai.service';
 import { UserContextService } from 'src/user-context/user-context.service';
 import { AmadeusService } from 'src/amadeus/amadeus.service';
 import { HttpModule } from '@nestjs/axios';
+import { OpenaiController } from './openai.controller';
 
 @Module({
   imports: [HttpModule],
+  controllers: [OpenaiController],
   providers: [OpenaiService, UserContextService, AmadeusService],
 })
 export class OpenaiModule {}
