a47f4f47ec5e978bc98493e217e65494397ab7cd
Abdullah Habberrih <abdul2013ly@gmail.com>
2025-03-27 11:54:41 +0200

    refactor: added some modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,18 @@ import { ConfigModule } from '@nestjs/config';
 import { OpenaiModule } from './openai/openai.module';
 import { UserContextModule } from './user-context/user-context.module';
 import { AmadeusModule } from './amadeus/amadeus.module';
+import { PaymentModule } from './payment/payment.module';
+import { StripeWebhookModule } from './payment/webhook/webhook.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    PaymentModule.forRootAsync(),
     WhatsappModule,
     OpenaiModule,
     UserContextModule,
     AmadeusModule,
+    StripeWebhookModule,
   ],
   controllers: [AppController],
   providers: [AppService],
