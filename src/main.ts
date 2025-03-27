a75967d6b2054735a937ed3608f4386b85d75713
Abdullah Habberrih <abdul2013ly@gmail.com>
2025-03-27 11:56:12 +0200

    refactor: make req for stripe webhook as body raw

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // This is for Stripe Webhook
+  app.use('/api/stripe-webhook', bodyParser.raw({ type: 'application/json' }));
+
+  app.enableCors();
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   await app.listen(3000);
 }
 bootstrap();
