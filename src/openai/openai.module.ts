8b75cfc6775c53014daa645a3e76a133866a1a66
Abdullah Habberrih <abdul2013ly@gmail.com>
2025-03-29 13:58:24 +0200

    refactor: define the payment service in other modules

--- src/openai/openai.module.ts
+++ src/openai/openai.module.ts
@@ -4,10 +4,16 @@ import { UserContextService } from 'src/user-context/user-context.service';
 import { AmadeusService } from 'src/amadeus/amadeus.service';
 import { HttpModule } from '@nestjs/axios';
 import { OpenaiController } from './openai.controller';
+import { PaymentService } from 'src/payment/payment.service';
 
 @Module({
   imports: [HttpModule],
   controllers: [OpenaiController],
-  providers: [OpenaiService, UserContextService, AmadeusService],
+  providers: [
+    OpenaiService,
+    UserContextService,
+    AmadeusService,
+    PaymentService,
+  ],
 })
 export class OpenaiModule {}
--- src/whatsapp/whatsapp.module.ts
+++ src/whatsapp/whatsapp.module.ts
@@ -5,10 +5,17 @@ import { OpenaiService } from 'src/openai/openai.service';
 import { UserContextService } from '../user-context/user-context.service';
 import { AmadeusService } from 'src/amadeus/amadeus.service';
 import { HttpModule } from '@nestjs/axios';
+import { PaymentService } from 'src/payment/payment.service';
 
 @Module({
   imports: [HttpModule],
   controllers: [WhatsappController],
-  providers: [OpenaiService, WhatsappService, UserContextService, AmadeusService],
+  providers: [
+    OpenaiService,
+    WhatsappService,
+    UserContextService,
+    AmadeusService,
+    PaymentService,
+  ],
 })
 export class WhatsappModule {}
